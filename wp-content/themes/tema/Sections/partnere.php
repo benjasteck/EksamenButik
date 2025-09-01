46f0d6cede6899ccc6f2400e3a10995c2144e831
benjasteck <benj5102@easv365.dk>
2025-09-01 11:06:26 +0200

    partnere

---
+++ wp-content/themes/tema/Sections/partnere.php
@@ -0,0 +1,41 @@
+<section id="partnere">
+<h2 class="partnerTitle">Partnere</h2>
+<div class="logoLine">
+    <div class="logoSlide">
+    <img src="images/logo-melpe-branco.png" alt="">
+    <img src="images/leo-hvid.png" alt="">
+    <img src="images/skat-hvid.png" alt="">
+    <img src="images/nordic-event.png" alt="">
+    <img src="images/klarlund-logo.png" alt="">
+    <img src="images/maersk-group-logo-hvid.png" alt="">
+    <img src="images/reme-1000.png" alt="">
+    <img src="images/telenor-white-logo-300x152-1.png" alt="">
+    <img src="images/novo-hvid.png" alt="">
+    <img src="images/tdc-hvid.png" alt="">
+    <img src="images/pandora-logo.png" alt="">
+    <img src="images/dtu-white-2.png" alt="">
+    <img src="images/sdu-hvid.png" alt="">
+    <img src="images/rigshospitalet-hvid.png" alt="">
+    <img src="images/cowi-hvid.png" alt="">
+    <img src="images/danske-bank-hvid.png" alt="">
+    </div>
+    <div class="logoSlide">
+    <img src="images/logo-melpe-branco.png" alt="">
+    <img src="images/leo-hvid.png" alt="">
+    <img src="images/skat-hvid.png" alt="">
+    <img src="images/nordic-event.png" alt="">
+    <img src="images/klarlund-logo.png" alt="">
+    <img src="images/maersk-group-logo-hvid.png" alt="">
+    <img src="images/reme-1000.png" alt="">
+    <img src="images/telenor-white-logo-300x152-1.png" alt="">
+    <img src="images/novo-hvid.png" alt="">
+    <img src="images/tdc-hvid.png" alt="">
+    <img src="images/pandora-logo.png" alt="">
+    <img src="images/dtu-white-2.png" alt="">
+    <img src="images/sdu-hvid.png" alt="">
+    <img src="images/rigshospitalet-hvid.png" alt="">
+    <img src="images/cowi-hvid.png" alt="">
+    <img src="images/danske-bank-hvid.png" alt="">
+    </div>
+</div>
+</section>
