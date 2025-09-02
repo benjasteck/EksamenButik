1685e21b7203f53d375b1575cad10b2b54d0e90a
benjasteck <benj5102@easv365.dk>
2025-09-02 11:19:12 +0200

    Update partnere.php

--- wp-content/themes/tema/Sections/partnere.php
+++ wp-content/themes/tema/Sections/partnere.php
@@ -1,24 +1,30 @@
+<?php
+$args = array(
+    'post_type' => 'partner', // Replace 'partner' with your actual custom post type name
+    'posts_per_page' => -1,  // Get all partner posts
+    'orderby' => 'menu_order',
+    'order' => 'ASC',
+);
+
+$partner_logos = get_posts($args);
+?>
+
 <section id="partnere">
 <h2 class="partnerTitle">Partnere</h2>
-<div class="logoLine">
-    <div class="logoSlide">
-    <img src="images/logo-melpe-branco.png" alt="">
-    <img src="images/leo-hvid.png" alt="">
-    <img src="images/skat-hvid.png" alt="">
-    <img src="images/nordic-event.png" alt="">
-    <img src="images/klarlund-logo.png" alt="">
-    <img src="images/maersk-group-logo-hvid.png" alt="">
-    <img src="images/reme-1000.png" alt="">
-    <img src="images/telenor-white-logo-300x152-1.png" alt="">
-    <img src="images/novo-hvid.png" alt="">
-    <img src="images/tdc-hvid.png" alt="">
-    <img src="images/pandora-logo.png" alt="">
-    <img src="images/dtu-white-2.png" alt="">
-    <img src="images/sdu-hvid.png" alt="">
-    <img src="images/rigshospitalet-hvid.png" alt="">
-    <img src="images/cowi-hvid.png" alt="">
-    <img src="images/danske-bank-hvid.png" alt="">
-    </div>
+<div class="logoSlide">
+            <?php
+            if ($partner_logos) {
+                foreach ($partner_logos as $post) {
+                    setup_postdata($post);
+                    $logo = get_field('partner_logo_img'); // Use the name of your ACF image field
+                    if ($logo) {
+                        echo '<img src="' . esc_url($logo['url']) . '" alt="' . esc_attr($logo['alt']) . '" />';
+                    }
+                }
+                wp_reset_postdata();
+            }
+            ?>
+        </div>
     <div class="logoSlide">
     <img src="images/logo-melpe-branco.png" alt="">
     <img src="images/leo-hvid.png" alt="">
