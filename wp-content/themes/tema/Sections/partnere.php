a12be0c875f403ac5761e810a60568098a0cbfd1
benjasteck <benj5102@easv365.dk>
2025-09-02 11:29:58 +0200

    Update partnere.php

--- wp-content/themes/tema/Sections/partnere.php
+++ wp-content/themes/tema/Sections/partnere.php
@@ -3,42 +3,22 @@
     <h2 class="partnerTitle">Partnere</h2>
     <div class="logoLine">
         <?php
-        $args = array(
-            'post_type'      => 'partner',
-            'posts_per_page' => -1,
-            'orderby'        => 'menu_order',
-            'order'          => 'ASC',
-        );
+        $images = get_field('partner_logo_img'); // 'partner_logo_img' is your ACF Gallery field name
 
-        $partner_logos = get_posts($args);
-
-        if ($partner_logos) {
-            // First logoSlide div
-            echo '<div class="logoSlide">';
-            foreach ($partner_logos as $post) {
-                setup_postdata($post);
-                // Correct field name here
-                $logo = get_field('partner_logo_img'); 
-                if ($logo) {
-                    echo '<img src="' . esc_url($logo['url']) . '" alt="' . esc_attr($logo['alt']) . '" />';
-                }
-            }
-            echo '</div>';
-            wp_reset_postdata();
-
-            // Second logoSlide div
-            echo '<div class="logoSlide">';
-            foreach ($partner_logos as $post) {
-                setup_postdata($post);
-                // Correct field name here
-                $logo = get_field('partner_logo_img'); 
-                if ($logo) {
-                    echo '<img src="' . esc_url($logo['url']) . '" alt="' . esc_attr($logo['alt']) . '" />';
-                }
-            }
-            echo '</div>';
-            wp_reset_postdata();
-        }
-        ?>
+        if ($images) : ?>
+            
+            <div class="logoSlide">
+                <?php foreach ($images as $image) : ?>
+                    <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                <?php endforeach; ?>
+            </div>
+            
+            <div class="logoSlide">
+                <?php foreach ($images as $image) : ?>
+                    <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                <?php endforeach; ?>
+            </div>
+            
+        <?php endif; ?>
     </div>
 </section>
