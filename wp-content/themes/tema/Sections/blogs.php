98358a24a307b63b5080c8df9889b9ccac9810a6
benjasteck <benj5102@easv365.dk>
2025-09-09 09:32:18 +0200

    Update blogs.php

--- wp-content/themes/tema/Sections/blogs.php
+++ wp-content/themes/tema/Sections/blogs.php
@@ -4,45 +4,7 @@ $blogBaggrund = get_field("blog_background");
 ?>
 
 <section id="blogs">
-    <div class="blogContainer">
-            <div class="blogContent">
-                <div class="stickycontainer">
-                    <div class="blogImg">
-                        <div class="stickyImg2">
-                            <img src="images/blogImg1.jpg" alt="">
-                        </div>
-                        <div class="stickyImg2">
-                             <img src="images/blogImg2.jpg" alt="">       
-                        </div>
-                        <div class="stickyImg2">
-                            <img src="images/blogImg3.jpg" alt="">
-                        </div>
-                        <div class="stickyImg2">
-                             <img src="images/blogImg4.jpg" alt="">   
-                        </div>
-                </div>
-            </div>
-            <div class="blogText">
-                <div class="blogTextContent">
-                <div class="progressbar">
-                    <div class="progressbarInner">
-
-                    </div>
-                </div>
-                <h1 class="blogTitel">Lej DJ hos Waveform</h1>
-                <div class="tagContainer">
-                    <div class="tag"><p>#DJ</p></div>
-                    <div class="tag"><p>#Leje</p></div>
-                    <div class="tag"><p>#DinMor</p></div>
-                </div>
-                <h4 class="blogSnippet">Forskning og flere studier viser at konventionel medicin oprindeligt brugt til andre lidelser kan hjælpe med at sulte kræft og i sidste ende slå kræftceller ihjel.</h4>
-                <div class="blogBtn">
-                    <p>Læs Mere</p>
-                </div>
-                </div>
-            </div>
-            </div>
-    </div>
+    <?php get_template_part( '/home' ); ?>
     <h1 class="blogBottomTitle">Vi klarer alt det praktiske og tekniske</h1>
     <h4 class="BlogBottomSubTitle">Forskning og flere studier viser at konventionel medicin oprindeligt brugt til andre lidelser kan hjælpe med at sulte kræft og i sidste ende slå kræftceller ihjel.</h4>
     <div class="blogBottomBtn">
