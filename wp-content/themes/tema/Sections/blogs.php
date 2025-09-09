af29ecc7377aeebae2e5c03bbff16298f694a308
benjasteck <benj5102@easv365.dk>
2025-09-09 09:21:47 +0200

    blogs

--- wp-content/themes/tema/Sections/blogs.php
+++ wp-content/themes/tema/Sections/blogs.php
@@ -4,22 +4,32 @@ $blogBaggrund = get_field("blog_background");
 ?>
 
 <section id="blogs">
-    <div class="blogContainer">
-            <div class="blogContent">
+    <?php if(have_posts()): ?>
+        <?php
+        $post_counter = 1; 
+        $top_value = 10;
+        ?>
+		<?php while(have_posts()): the_post() ?>
+			<?php
+			$url = get_the_permalink();
+			$title = get_the_title();
+			$excerpt = get_the_excerpt();
+			$categories = get_the_category();
+			$tags = get_the_tags();
+            $image = get_field("blogimg");
+            $post_id = "post" . $post_counter;
+            
+			?>
+			
+
+            
+            <div class="blogContent"  id="<?php echo esc_attr($post_id); ?>">
                 <div class="stickycontainer">
-                    <div class="blogImg">
-                        <div class="stickyImg2">
-                            <img src="images/blogImg1.jpg" alt="">
+                    <div class="blogImg"  style="top: <?php echo esc_attr($top_value); ?>px;">
+                        <div class="stickyImg2" >
+                            <img src="<?php echo esc_url($image["url"]); ?>" alt="">
                         </div>
-                        <div class="stickyImg2">
-                             <img src="images/blogImg2.jpg" alt="">       
-                        </div>
-                        <div class="stickyImg2">
-                            <img src="images/blogImg3.jpg" alt="">
-                        </div>
-                        <div class="stickyImg2">
-                             <img src="images/blogImg4.jpg" alt="">   
-                        </div>
+                        
                 </div>
             </div>
             <div class="blogText">
@@ -29,20 +39,32 @@ $blogBaggrund = get_field("blog_background");
 
                     </div>
                 </div>
-                <h1 class="blogTitel">Lej DJ hos Waveform</h1>
+                <h1 class="blogTitel"><?php echo esc_html($title); ?></h1>
                 <div class="tagContainer">
-                    <div class="tag"><p>#DJ</p></div>
-                    <div class="tag"><p>#Leje</p></div>
-                    <div class="tag"><p>#DinMor</p></div>
+                    <?php if($tags): ?>
+									<?php foreach($tags as $tag): ?>
+										<a href="<?php echo get_tag_link($tag->term_id); ?>" class="tag"><?php echo $tag->name; ?></a>
+									<?php endforeach; ?>
+								<?php endif; ?>
                 </div>
-                <h4 class="blogSnippet">Forskning og flere studier viser at konventionel medicin oprindeligt brugt til andre lidelser kan hjælpe med at sulte kræft og i sidste ende slå kræftceller ihjel.</h4>
+                <h4 class="blogSnippet"><?php echo esc_html($excerpt); ?></h4>
                 <div class="blogBtn">
-                    <p>Læs Mere</p>
+                    <a href="<?php echo esc_url($url); ?>">Læs Mere</a>
                 </div>
                 </div>
             </div>
             </div>
-    </div>
+   
+
+          <?php 
+          $post_counter++;
+          $top_value += 10;
+          ?>  
+                                       
+		<?php endwhile; ?>
+		
+	<?php endif; ?>
+</div>
     <h1 class="blogBottomTitle">Vi klarer alt det praktiske og tekniske</h1>
     <h4 class="BlogBottomSubTitle">Forskning og flere studier viser at konventionel medicin oprindeligt brugt til andre lidelser kan hjælpe med at sulte kræft og i sidste ende slå kræftceller ihjel.</h4>
     <div class="blogBottomBtn">
