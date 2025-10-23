81d190bf2a1c5103bb299cb86dea16dd1a3fa1bf
benjasteck <benj5102@easv365.dk>
2025-10-23 14:35:07 +0200

    konfirmationsfest

---
+++ wp-content/themes/tema/page-konfirmations-fest.php
@@ -0,0 +1,32 @@
+<?php get_header(); ?>
+<?php $ForsideHeaderTitel = get_field("subpageheader_titel");  ?>
+
+<style>
+#header {
+    height: 66vh;
+}
+
+.button-wrapper {
+    display: none;
+}
+
+.main-container {
+    height: 85vh;
+}
+</style>
+
+<div class="product-listing-area">
+    <?php 
+    // Option 1: Call the reusable function directly, specifying the tag slug
+    // We are calling render_products_by_tag() and echoing its output.
+    echo render_products_by_tag( 'KonfirmationsFest' ); 
+    ?>
+</div>
+</div>
+</div>
+</div>
+<?php get_template_part( 'Sections/blogs' ); ?>
+<script>
+
+</script>
+<?php get_footer() ?>
